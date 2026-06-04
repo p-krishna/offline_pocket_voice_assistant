21d5659f0a55633ea634deac9aa1950025166f33
p-krishna <krishna21.7.97@outlook.com>
2026-06-04 22:37:16 +0530

    revert from mtp to orig server 2026-06-04 22:36

--- src/llm/serve_gemma.sh
+++ src/llm/serve_gemma.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-"/home/puli/projects/local llm/llama.cpp.mtp/build/bin/llama-server" \
-  --no-ui \
+"/home/puli/projects/local llm/llama.cpp/build/bin/llama-server" \
+  --no-webui \
   -m "/home/puli/projects/local llm/models.gguf/gemma-4-E2B-it-UD-IQ2_M.gguf" \
   --reasoning off \
   --predict 150 \
